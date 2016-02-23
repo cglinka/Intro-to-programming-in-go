f9f3259c8b213c04f8b3734e8b2626a1d0333598
Clare Glinka <clare.glinka@returnpath.com>
2016-02-23 11:37:33 -0600

    Add adtl 'sender' function that sends 'pong'

--- concurrency2.go
+++ concurrency2.go
@@ -13,6 +13,12 @@ func pinger(c chan string) {
 	}
 }
 
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
 func printer(c chan string) {
 	for {
 		msg := <-c
@@ -25,6 +31,7 @@ func main() {
 	var c chan string = make(chan string)
 
 	go pinger(c)
+	go ponger(c)
 	go printer(c)
 
 	var input string
